b7fb32e6c32c112a4e2fe5d19683b17212f8827b
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2025-12-01 23:57:43 +0530

    starter files

---
+++ poup.js
@@ -0,0 +1,13 @@
+document.getElementById("summarize").addEventListener("click", () => {
+    const result = document.getElementById("result");
+    result.textContent = "Extracting text...";
+
+    chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
+        chrome.tabs.sendMessage(tab.id, { type: "GET_ARTICLE_TEXT" }, ({ text }) => {
+            result.textContent = text ? text.slice(0, 300) + "_": "No article text found.";
+        });
+    });
+
+    
+});
+
